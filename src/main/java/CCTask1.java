fd1ad82df961de784258357a40dd2af9936b07ea
Remport Balázs <remport.balazs@alerant.hu>
2022-02-14 10:09:20 +0100

    működik, de ronda

--- src/main/java/CCTask1.java
+++ src/main/java/CCTask1.java
@@ -34,14 +34,43 @@ public class CCTask1 {
 
         Iterator<String> itr = l.iterator();
         String sor = null;
+        Float minimum = Float.MAX_VALUE;
+        String minSor = null;
+        Float min = null;
+        Float max = null;
+
+        int i = 0;
         while (itr.hasNext()) {
             sor = itr.next();
+
+            if (i == 0 || i == 1) {
+                i++;
+                continue;
+            }
+            i++;
+
+
+            sor = sor.trim();
             sor = sor.replaceAll("( )+", " ");
             System.out.println(sor);
 
             String[] splitString = sor.split(" ");
-            System.out.println("Elem0:" + splitString[0]);
-            
+            String sorszam = splitString[0];
+            try {
+                max = Float.valueOf(splitString[1]);
+                min = Float.valueOf(splitString[2]);
+            } catch (Exception e) {
+                continue;
+            }
+
+            Float kulonbseg = max - min;
+            if (kulonbseg < minimum) {
+                minimum = kulonbseg;
+                minSor = sorszam;
+            }
+            System.out.println("Min:" + minimum);
+            System.out.println("Min Sor:" + minSor);
+
         }
 
     }
